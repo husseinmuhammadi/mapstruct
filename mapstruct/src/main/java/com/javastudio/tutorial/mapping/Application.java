15c7adff85368d0a481cc0be4a4ba26da8d154d6
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-27 20:57:43 +0200

    mapsatruc

---
+++ mapstruct/src/main/java/com/javastudio/tutorial/mapping/Application.java
@@ -0,0 +1,12 @@
+package com.javastudio.tutorial.mapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Application started!");
+    }
+}
