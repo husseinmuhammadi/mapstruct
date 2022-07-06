2d6e11f921ab3360d9619792d6c37db96048b583
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-07-06 22:01:12 +0200

    mapper

---
+++ mapstruct/src/main/java/com/javastudio/tutorial/mapping/model/Product.java
@@ -0,0 +1,10 @@
+package com.javastudio.tutorial.mapping.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    String productName;
+    String productType;
+    String manufacturer;
+}
